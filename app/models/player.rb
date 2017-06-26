3fcd0680b2e7fcf7e4c890caec995038b39e09af
swcraig <sebastian@stembolt.com>
2017-06-26 10:45:51 -0700

    Link players to the games they have played
    Games must keep track of who is `player_one` (white), and who is
    `player_two` (black). This means that the all the games that a player
    has been involved in are the combination of all the games in which they
    were `player_one`, and all the games in which they were `player_two`.
    
    This commit adds `Player#games`, to combine these two Relations in order
    to get all of their games.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,9 @@
 class Player < ApplicationRecord
-  has_and_belongs_to_many :games
   has_many :moves
 
   validates :username, presence: true
+
+  def games
+    Game.where('player_one_id = :id OR player_two_id = :id', { id: id })
+  end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -6,4 +6,13 @@ describe Player, type: :model do
   it 'has a valid factory' do
     expect(subject).to be_valid
   end
+
+  describe '#games' do
+    let(:other_player) { create(:player) }
+    let!(:game) { create(:game, player_one: subject, player_two: other_player) }
+
+    it 'references all of the games they have played' do
+      expect(subject.games.first).to eq(game)
+    end
+  end
 end
