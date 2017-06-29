bce4244bf3296b5a31717ae350836a53a575832d
swcraig <sebastian@stembolt.com>
2017-06-29 11:12:00 -0700

    Add validations for `Move`
    Source and destination cells must be strings of length two, that are
    valid algebraic representations of cells. (i.e 'e2', 'a7', etc.)
    
    More info: https://en.wikipedia.org/wiki/Algebraic_notation_(chess)

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,4 +1,8 @@
 class Move < ApplicationRecord
   belongs_to :game
   belongs_to :player
+
+  validates :move_number, numericality: { only_integer: true, greater_than: 0 }
+  validates :source, :destination, format: { with: /\A[a-h]{1}[1-8]{1}\z/ }
+  validates :game, :player, presence: true
 end
--- spec/models/move_spec.rb
+++ spec/models/move_spec.rb
@@ -5,7 +5,7 @@ describe Move, type: :model do
   let(:player_two) { create(:player) }
   let(:game) { create(:game, player_one: player_one, player_two: player_two) }
 
-  subject { create(:move, player: player_one, game: game) }
+  subject { build(:move, player: player_one, game: game) }
 
   it 'has a valid factory' do
     expect(subject).to be_valid
@@ -17,5 +17,50 @@ describe Move, type: :model do
 
   it 'references the game properly' do
     expect(subject.game).to eq(game)
+  end
+
+  it 'is invalid without a move number' do
+    subject.move_number = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is invalid without a source cell' do
+    subject.source = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is invalid with an improper source cell' do
+    subject.source = 'f9'
+    expect(subject).not_to be_valid
+  end
+
+  it 'is valid with a source cell that is on the board' do
+    subject.source = 'c8'
+    expect(subject).to be_valid
+  end
+
+  it 'is invalid without a destination cell' do
+    subject.destination = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is invalid with an improper destination cell' do
+    subject.destination = 'a73'
+    expect(subject).not_to be_valid
+  end
+
+  it 'is valid with a destination cell that is on the board' do
+    subject.destination = 'a1'
+    expect(subject).to be_valid
+  end
+
+  it 'is invalid without a game' do
+    subject.game = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is invalid without a moving player' do
+    subject.player = nil
+    expect(subject).not_to be_valid
   end
 end
