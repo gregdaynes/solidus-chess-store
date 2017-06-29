dcd68aacbc133edd40ec3e4bc7e97769c2d44eea
swcraig <sebastian@stembolt.com>
2017-06-29 12:18:01 -0700

    Validate uniqueness on `Player#username`
    Although there won't be any user authentication anytime soon, we do not
    want multiple Player's with the same username.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,7 @@
 class Player < ApplicationRecord
   has_many :moves
 
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
 
   def games
     Game.where('player_one_id = :id OR player_two_id = :id', { id: id })
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -12,6 +12,12 @@ describe Player, type: :model do
     expect(subject).not_to be_valid
   end
 
+  it 'does not allow duplicate usernames' do
+    player = create(:player)
+    subject.username = player.username
+    expect(subject).not_to be_valid
+  end
+
   describe '#games' do
     let(:other_player) { create(:player) }
     let!(:game) { create(:game, player_one: subject, player_two: other_player) }
